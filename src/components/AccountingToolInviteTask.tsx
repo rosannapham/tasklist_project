a1bee3085b02a267a766a5eeb49db4825ce4a651
Rosanna Pham <rosannapham@Rosannas-MacBook-Pro.local>
2026-03-26 10:59:34 +0000

    wip:accounting tool invite

--- src/components/AccountingToolInviteTask.tsx
+++ src/components/AccountingToolInviteTask.tsx
@@ -4,16 +4,42 @@ import { FullScreenModal } from "./ui/FullScreenModal";
 import { useRouter } from "next/navigation";
 import { useAccountingToolInviteTask } from "@/hooks/useAccountingToolInviteTask";
 
+function XeroOptionContent() {
+    return<Text>Xero</Text>
+}
+
+function QuickBooksOptionContent() {
+    return<Text>Quickbooks</Text>
+}
+
+function OtherOptionContent() {
+    return<Text>Other</Text>
+}
+function NoAccountingToolOptionContent() {
+    return<Text>no tool</Text>
+}
+function EmptyContent() {
+    return<></>
+}
+const OPTION_CONTENT: Record<number, React.ComponentType> = {
+    1: XeroOptionContent,
+    2: QuickBooksOptionContent,
+    3: OtherOptionContent,
+    4: NoAccountingToolOptionContent
+}
+
+
 export function AccountingToolInviteTask() {
     const router = useRouter()
     const {task, handleSelectCard, selectedId, options } = useAccountingToolInviteTask();
+    const SelectedContent = selectedId ? OPTION_CONTENT[selectedId] : EmptyContent
     return (
     <FullScreenModal onClose={() => {router.push(`/tasks`)}} title={"Invite Novabook to your accounting tool"}>
           <Box className="max-w-md mx-auto p-4 border rounded-lg bg-white">
         <Flex direction="column" gap ="4" >
           
         <Text as="div" size="3" weight="bold" className="mb-4">
-        Do you want to proceed?
+        Which accounting tool do you use?
       </Text>
          {options.map((option) => {
 
@@ -28,6 +54,10 @@ export function AccountingToolInviteTask() {
     </Flex>
     {selectedId}
   </Box>
+
+  { SelectedContent && (
+    <SelectedContent/>
+  )}
 
     </FullScreenModal>
     )
