24aa23ddd9800987def3269398ee51a937d6ded7
Rosanna Pham <rosannapham@Rosannas-MacBook-Pro.local>
2026-03-26 22:32:20 +0000

    adding to readme

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,5 +1,7 @@
+"use client";
+import React from 'react';
 import toast from 'react-hot-toast';
-
+import { SuccessToast, ErrorToast } from '@/components/ui/Toast';
 
 export function useToast() {
   const showSuccess = (message: string) => {
