b1d9f115acd15c480a4747555be63a95df4e6840
Rosanna Pham <rosannapham@Rosannas-MacBook-Pro.local>
2026-03-26 18:39:44 +0000

    wip:accounting tool invite

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,5 +1,5 @@
 import { Cross2Icon } from '@radix-ui/react-icons';
-import { Callout, Flex, IconButton } from '@radix-ui/themes';
+import { Badge, Callout, Flex, IconButton } from '@radix-ui/themes';
 
 interface ToastProps {
   type: 'success' | 'error';
@@ -9,12 +9,14 @@ interface ToastProps {
 
 export function Toast({ type, message, onClose }: ToastProps) {
   return (
-    <div className="flex h-screen w-full p-4 justify-center items-end">
-    <Callout.Root
+    <div className="flex h-screen w-full p-4 justify-center items-end bg-red-500">
+    <div className=" transform -translate-x-1/2 z-[9999] rounded-full 
+        animate-in slide-in-from-bottom-full bg-red-500">
+    <Badge 
     highContrast
-      color={type === 'success' ? 'green' : 'red'}
-      className=" transform -translate-x-1/2 z-[9999] rounded-lg 
-        animate-in slide-in-from-bottom-full"
+    size="3"
+    radius='full'
+      
     >
   <Flex direction="row" gap='2' align="center">
       <Callout.Text>
@@ -24,7 +26,8 @@ export function Toast({ type, message, onClose }: ToastProps) {
        <Cross2Icon/>
       </IconButton>
       </Flex>
-    </Callout.Root>
+      </Badge>
+    </div>
     </div>
   );
 }
